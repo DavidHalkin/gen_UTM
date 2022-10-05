2b6622421b009a321b46f81254bd905e3fa29306
David Halkin <halkin.david@gmail.com>
2022-10-05 14:15:03 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+	// <i class="far fa-eye-slash"></i>
+	// const fieldPass = document.querySelectorAll('.input_holder_js');
+	// fieldPass.forEach(element => {
+
+	// 	element.addEventListener('click')
+	// });
+});
