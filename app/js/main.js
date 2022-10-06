1fa7a712a67a811c762f1c7bb59f74992a1dc161
David Halkin <halkin.david@gmail.com>
2022-10-06 18:32:17 +0300

    123

--- app/js/main.js
+++ app/js/main.js
@@ -6,36 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// 	element.addEventListener('click')
 	// });
+	$(".showComment_js").click(function () {
+		$(this).parents(".form-group").find('.hidden_text').slideToggle('fast');
+	});
 });
 
-
-// (function (document, window, index) {
-// 	var inputs = document.querySelectorAll('.inputfile');
-// 	Array.prototype.forEach.call(inputs, function (input) {
-// 		var label = input.nextElementSibling,
-// 			labelVal = label.innerHTML;
-
-// 		input.addEventListener('change', function (e) {
-// 			var fileName = '';
-// 			if (this.files && this.files.length > 1) {
-// 				fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-// 			} else {
-// 				fileName = e.target.value.split('\\').pop();
-// 			}
-
-// 			if (fileName) {
-// 				label.querySelector('span').innerHTML = fileName;
-// 			} else {
-// 				label.innerHTML = labelVal;
-// 			}
-// 		});
-
-// 		// Firefox bug fix
-// 		input.addEventListener('focus', function () {
-// 			input.classList.add('has-focus');
-// 		});
-// 		input.addEventListener('blur', function () {
-// 			input.classList.remove('has-focus');
-// 		});
-// 	});
-// }(document, window, 0));
+// $(function () {
+// 	console.log("ready!");
+// });
